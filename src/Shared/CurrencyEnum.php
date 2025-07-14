3ff90c158c5b4c1a9da509536f785f4763d4527d
Paweł Chomiczewski <ipcsoftwaredev@gmail.com>
2025-07-14 14:49:36 +0200

    create CurrencyEnum & Money

---
+++ src/Shared/CurrencyEnum.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace TeamConnect\Shared;
+
+enum CurrencyEnum: string
+{
+    case Pln = 'PLN';
+    case Eur = 'EUR';
+}
---
+++ src/Shared/Money.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace TeamConnect\Shared;
+
+class Money
+{
+    private int $amount;
+    private CurrencyEnum $currency;
+
+    private function __construct(int $amount, CurrencyEnum $currency)
+    {
+        if (!in_array($currency, [CurrencyEnum::Pln, CurrencyEnum::Eur])) {
+            throw new \InvalidArgumentException('Invalid currency');
+        }
+
+        $this->amount = $amount;
+        $this->currency = $currency;
+    }
+
+    public static function create(int $amount, $currency): self
+    {
+        return new self($amount, $currency);
+    }
+
+    public function getAmount(): int
+    {
+        return $this->amount;
+    }
+
+    public function getCurrency(): CurrencyEnum
+    {
+        return $this->currency;
+    }
+}
